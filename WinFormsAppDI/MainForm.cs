772a517fb5d51f90b6640941863567d951ba4bee
zeraati <zeraatimail@gmail.com>
2022-09-12 23:47:27 +0430

    init

--- WinFormsAppDI/MainForm.cs
+++ WinFormsAppDI/MainForm.cs
@@ -1,8 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace WinFormsAppDI
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
